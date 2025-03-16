370e6a2c72d5e29f0b85e718c1661713d9b65513
Oleg <olegmaslakov47@gmail.com>
2025-03-16 11:34:32 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function factorial(n) {
+  if (n < 0 || typeof n !== 'number') {
+    return 'Error';
+  }
+  if (n === 1 || n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(5));
+console.log(factorial(-5));
+console.log(factorial(0));
+console.log(factorial('qewrere'));
+console.log(factorial(1));
