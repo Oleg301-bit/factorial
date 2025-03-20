fc09a85793b7f16ffb0fb428ac739b4c66a43dd4
Oleg <olegmaslakov47@gmail.com>
2025-03-21 00:49:58 +0200

    Add some changes

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ function factorial(n) {
   }
   if (n === 1 || n === 0) {
     return 1;
-  } else {
-    return n * factorial(n - 1);
   }
+  return n * factorial(n - 1);
 }
 console.log(factorial(5));
 console.log(factorial(-5));
